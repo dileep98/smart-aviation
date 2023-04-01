08a58dc8e52c1e663381248667d96a3bb2971069
dileep98 <dk26092@gmail.com>
2023-04-01 02:50:52 -0400

    init changes

---
+++ src/test/java/com/sv/smartaviation/SmartAviationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.smartaviation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAviationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
